c1f167580b20a6e51c33f1a8f23f3b46318867bf
Atharv-ND <atharvnaikdesai@gmail.com>
2025-08-03 12:24:40 +0530

    middleware added

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,17 +1,5 @@
 const Product = require("../models/Product");
-let cachedProducts = [];
-let productsLoaded = false;
-
 // Preload products at server startup
-async function preloadProducts() {
-  try {
-    cachedProducts = await Product.find();
-    productsLoaded = true;
-    console.log("Products preloaded in cache.");
-  } catch (err) {
-    console.error("Failed to preload products:", err);
-  }
-}
 
 // Get all products or find a specific product by ID
 const getProducts = async (req, res) => {
@@ -20,27 +8,16 @@ const getProducts = async (req, res) => {
 
     switch (action) {
       case "getProducts": {
-        // Serve from cache if loaded
-        if (productsLoaded) {
-          return res.status(200).json({ products: cachedProducts });
-        } else {
           const products = await Product.find();
           return res.status(200).json({ products });
-        }
       }
 
       case "findProduct": {
         if (!id) {
           return res.status(400).json({ message: "Product ID is required." });
         }
-        // Try to find in cache first
-        let product = null;
-        if (productsLoaded) {
-          product = cachedProducts.find((p) => p.product_id === id);
-        }
-        if (!product) {
-          product = await Product.findOne({ product_id: id });
-        }
+        product = await Product.findOne({ product_id: id });
+        
         if (!product) {
           return res.status(404).json({ message: "Product not found." });
         }
